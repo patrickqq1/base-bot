8b3028a431dc2fcd0d5d6a36740207f5ea4d89f9
Thiago Camelo <thiago.la1707@gmail.com>
2023-10-26 14:43:21 -0300

    olar mundo

--- src/rms.py
+++ src/rms.py
@@ -1,4 +1,4 @@
-import pyautogui
+import pyautogui as bot
 import time
 
 usuario = 'bot'
@@ -6,23 +6,22 @@ senha = 'abc@12345'
 
 def abrir_rms():
     path_rms = 'C:\\RMS\\WRMS\\SKY-SRVPRE-01\\PINHEIRO\\EXE\\WRMS.exe'
-    pyautogui.press('win')
-    pyautogui.write(path_rms)
+    bot.press('win')
+    bot.write(path_rms)
     time.sleep(2)
-    pyautogui.press('enter')
+    bot.press('enter')
     time.sleep(2)
-    pyautogui.write(usuario)
+    bot.write(usuario)
     time.sleep(1)
-    pyautogui.press('tab')
+    bot.press('tab')
     time.sleep(1)
-    pyautogui.write(senha)
-    pyautogui.press('enter')
+    bot.write(senha)
+    bot.press('enter')
     time.sleep(4)    
 
 def abrir_programa():
-        imagem1 = pyautogui.locateOnScreen('src/assets/recebimento.png', confidence=0.9, grayscale=True)
-        x = imagem1.x
-        y = imagem1.y
+        imagem1 = bot.locateOnScreen('src/assets/recebimento.png', confidence=0.9, grayscale=True)
+        x,y = bot.center(imagem1)
         print(x, y)
         print("deu bom!")
     
